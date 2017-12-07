ef66523ee674c321b07b14eb3970f30756d3c236
zhanghanke <zhanghanke_2004@163.com>
2017-12-07 19:48:34 +0800

    Update addTwoNumbers.go

--- addTwoNumbers.go
+++ addTwoNumbers.go
@@ -1,4 +1,15 @@
 /**
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+
+
 * 第一次，题目理解错误。再次感叹自己的水平实太水了..
  * Definition for singly-linked list.
  * type ListNode struct {
